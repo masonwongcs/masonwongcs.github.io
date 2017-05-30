ba3bb390afb2f51ed7cbf54322740c5d4f207bed
Seong6232 <Seong6232@1utar.my>
2017-05-30 13:43:08 +0800

    Updated main.js with hack user agent

--- rssMovie/js/main.js
+++ rssMovie/js/main.js
@@ -158,7 +158,7 @@ function getContent(url) {
         dataType: "jsonp",
         type: 'GET',
         async: true,
-        headers: {"User-Agent": "user-agent: "Mozilla/5.0 (iPhone; CPU iPhone OS 9_1 like Mac OS X) AppleWebKit/601.1.46 (KHTML, like Gecko) Version/9.0 Mobile/13B143 Safari/601.1"
+        headers: {"User-Agent": "Mozilla/5.0 (iPhone; CPU iPhone OS 9_1 like Mac OS X) AppleWebKit/601.1.46 (KHTML, like Gecko) Version/9.0 Mobile/13B143 Safari/601.1"
 "}
         
         // data: {
