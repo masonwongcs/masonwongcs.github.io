e857ab18dd5de5c60018628f1dd35ac0d21ce9d1
Mason Wong <Seong6232@gmail.com>
2020-03-02 13:28:21 +0800

    Remove all files and start again

--- backup/mpay2u/managepay/js/bgpos.js
+++
@@ -1,9 +0,0 @@
-updatePosition = function() {
-  var hero = document.getElementById('hero');
-  var scrollPos = window.pageYOffset / 2;
-  hero.style['background-position'] = '1% ' + scrollPos + 'px';
-};
-
-$(document).ready(function() {
-  $(window).on('scroll', updatePosition);
-});
--- backup/template/survey/js/main-4.js
+++
@@ -1,5 +0,0 @@
-$(document).ready(function () {
-    $('.ui.dropdown').dropdown();
-    $('.ui.rating').rating();
-    TweenMax.fromTo($('.container'), 0.4, {y: -200, alpha: 0}, {y: 0, alpha: 1})
-});
